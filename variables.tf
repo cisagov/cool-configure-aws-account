63066eb578edda256a438cf890abe34a211a9ffa
David Redmin <david.redmin@trio.dhs.gov>
2022-01-05 18:04:45 -0500

    Define initial variables

--- variables.tf
+++ variables.tf
@@ -3,10 +3,9 @@
 #
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
-
-variable "subnet_id" {
+variable "account_name_regex" {
   type        = string
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)."
+  description = "The Terraform regular expression matching the name of the account(s) that you want to configure (e.g. \"^[[:alnum:]]-production$\").  See https://www.terraform.io/language/functions/regex for details on Terraform regular expression syntax."
 }
 
 # ------------------------------------------------------------------------------
@@ -14,20 +13,27 @@ variable "subnet_id" {
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
-variable "ami_owner_account_id" {
-  type        = string
-  description = "The ID of the AWS account that owns the Example AMI, or \"self\" if the AMI is owned by the same account as the provisioner."
-  default     = "self"
-}
-
-variable "aws_availability_zone" {
-  type        = string
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)."
-  default     = "a"
-}
 
 variable "aws_region" {
   type        = string
   description = "The AWS region to deploy into (e.g. us-east-1)."
   default     = "us-east-1"
+}
+
+variable "groups_to_add_access_to" {
+  type        = list(object({ group = string, permission_sets = list(string) }))
+  description = "A list of objects specifying Single Sign-On (SSO) groups to add permissions to.  Each object contains the SSO group name and the list of permission sets to add access to.  Example: [{ group = \"Admins\", permission_sets = [\"AWSAdministratorAccess\"] }]"
+  default     = []
+}
+
+variable "users_to_remove_access_from" {
+  type        = list(object({ username = string, permission_sets = list(string) }))
+  description = "A list of objects specifying Single Sign-On (SSO) users to remove permissions from.  Each object contains the SSO username and the list of permission sets to remove access from.  Example: [{ username = \"john.doe@example.com\", permission_sets = [\"AWSAdministratorAccess\"] }]"
+  default     = []
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all AWS resources created."
+  default     = {}
 }
