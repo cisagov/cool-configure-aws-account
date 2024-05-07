1875914022ce49b187ce4a51138a4041867908ad
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-05-07 11:56:53 -0400

    Alphabetize

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "service_quotas" {
-  value       = aws_servicequotas_service_quota.all
   description = "All requested service quotas."
+  value       = aws_servicequotas_service_quota.all
 }
 
 output "sso_group_assignments" {
-  value       = aws_ssoadmin_account_assignment.group
   description = "The permission set assignments of each SSO group in each AWS account."
+  value       = aws_ssoadmin_account_assignment.group
 }
 
 output "sso_user_assignments" {
-  value       = aws_ssoadmin_account_assignment.user
   description = "The permission set assignments of each SSO user in each AWS account."
+  value       = aws_ssoadmin_account_assignment.user
 }
